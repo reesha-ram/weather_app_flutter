bd0e02bfdc49638ab7f4617798f8b359cbabe851
R REESHA <64142845+reesha-ram@users.noreply.github.com>
2021-05-29 20:37:45 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:weather_app1/screens/loading_screen.dart';
+
+import 'screens/city_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    // Forcing Portrait orientation in device for App
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          bodyText1: TextStyle(fontFamily: 'Montserrat'),
+        ),
+      ),
+      routes: {
+        '/CityScreen': (context) => CityScreen(),
+      },
+      home: LoadingScreen(),
+    );
+  }
+}
